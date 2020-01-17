2ee59c2bc31f3415d580fdcba5f307707c05264f
Sam Alws <samalws@gmail.com>
2020-01-17 02:43:41 +0000

    Add new file

---
+++ conf.nix
@@ -0,0 +1,35 @@
+{config, pkgs, ...}:
+{
+ imports = [
+  <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-base.nix>
+  <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+ ];
+ services.xserver = {
+  enable = true;
+  libinput.enable = true;
+  desktopManager.xterm.enable = false;
+  displayManager.auto = {
+        enable = true;
+        user = "nixos";
+  };
+  windowManager = {
+        dwm.enable = true;
+        default = "dwm";
+  };
+ };
+ networking.wireless = {
+  enable = true;
+  # networks. .psk = " ";
+ };
+ hardware.pulseaudio.enable = true;
+ environment.systemPackages = [
+  pkgs.ed
+  pkgs.st
+  pkgs.surf
+  pkgs.slock
+  pkgs.dmenu
+  pkgs.qemu
+  pkgs.pamixer
+  pkgs.upower
+ ];
+}
